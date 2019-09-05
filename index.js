1dcb879155a4d53d5f1243f70d8950593ff44560
Jaron Romijn <jaron@Jarons-MacBook-Pro-2.local>
2019-09-05 21:16:36 +0200

    done

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const mongoose = require('mongoose');
-const Recipe = require('./models/Recipe'); // Import of the model Recipe from './models/Recipe'
-const data = require('./data.js');  // Import of the data from './data.js'
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe"); // Import of the model Recipe from './models/Recipe'
+const data = require("./data.js"); // Import of the data from './data.js'
 
 // Connection to the database "recipeApp"
-mongoose.connect('mongodb://localhost/recipeApp', { useNewUrlParser: true })
+mongoose
+  .connect("mongodb://localhost/recipeApp", { useNewUrlParser: true })
   .then(() => {
-    console.log('Connected to Mongo!');
-  }).catch(err => {
-    console.error('Error connecting to mongo', err);
+    Recipe.deleteOne({ title: "Carrot Cake" })
+      .then(successCallback)
+      .catch(errorCallback);
+    console.log("Connected to Mongo!");
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
   });
 
+function successCallback() {
+  console.log("Succes boi!");
+  mongoose.connection.close();
+}
+
+function errorCallback() {
+  console.log("Damn u suck!");
+}
